878d146c2da7d01e2433119de41fa8017a652ed3
Tanya <tanyamakarova121290@gmail.com>
2026-07-16 18:47:38 +0300

    Update algo.h

--- submission/algo.h
+++ submission/algo.h
@@ -6,26 +6,26 @@ template<class T, class Comp>
 std::deque<T> Merge(const std::deque<T>& half1, const std::deque<T>& half2, const Comp& comparator) {
     std::deque<T> result;
 
-    size_t i = 0;
-    size_t j = 0;
+    auto i = half1.begin();
+    auto j = half2.begin();
 
-    while (i < half1.size() && j < half2.size()) {
-        if (comparator(half1[i], half2[j])) {
-            result.push_back(half1[i]);
+    while (i < half1.end() && j < half2.end()) {
+        if (comparator(*i, *j)) {
+            result.push_back(*i);
             ++i;
         } else {
-            result.push_back(half2[j]);
+            result.push_back(*j);
             ++j;
         }
     }
 
-    while (i < half1.size()) {
-        result.push_back(half1[i]);
+    while (i < half1.end()) {
+        result.push_back(*i);
         ++i;
     }
 
-    while (j < half2.size()) {
-        result.push_back(half2[j]);
+    while (j < half2.end()) {
+        result.push_back(*j);
         ++j;
     }
 
